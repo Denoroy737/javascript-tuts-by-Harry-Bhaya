1b9a3fc99771c8fc1e513508d9cb827761bc6bce
Kena737 <singhamitsboss@gmail.com>
2022-04-15 21:19:01 -0400

    Friday, 15 April 2022

---
+++ js/tut34.js
@@ -0,0 +1,11 @@
+console.log("Hey Don't worry we got The page")
+
+setTimeout(() => {
+    
+    for (let i = 0; i < 50; i++) {
+        const element = [i];
+        console.log("hello " + i)
+    }
+}, 100);
+
+console.log('done printing');
