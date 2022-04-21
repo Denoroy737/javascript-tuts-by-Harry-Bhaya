13cf03a50556b438b92c1eedf54f0d30d9cac606
Kena737 <singhamitsboss@gmail.com>
2022-04-20 20:31:25 -0400

    pratice set 5

--- word data fatching Website/tut38.js
+++ word data fatching Website/tut38.js
@@ -3,173 +3,34 @@
 // You have to print it in the dom
 // If you are using bootstrap then its a plus
 
-{
-    "word": "example",
-    "results": [
-      {
-        "definition": "a representative form or pattern",
-        "partOfSpeech": "noun",
-        "synonyms": [
-          "model"
-        ],
-        "typeOf": [
-          "representation",
-          "internal representation",
-          "mental representation"
-        ],
-        "hasTypes": [
-          "prefiguration",
-          "archetype",
-          "epitome",
-          "guide",
-          "holotype",
-          "image",
-          "loadstar",
-          "lodestar",
-          "microcosm",
-          "original",
-          "paradigm",
-          "pilot",
-          "prototype",
-          "template",
-          "templet",
-          "type specimen"
-        ],
-        "derivation": [
-          "exemplify"
-        ],
-        "examples": [
-          "I profited from his example"
-        ]
-      },
-      {
-        "definition": "something to be imitated",
-        "partOfSpeech": "noun",
-        "synonyms": [
-          "exemplar",
-          "good example",
-          "model"
-        ],
-        "typeOf": [
-          "ideal"
-        ],
-        "hasTypes": [
-          "pacemaker",
-          "pattern",
-          "beauty",
-          "prodigy",
-          "beaut",
-          "pacesetter"
-        ],
-        "derivation": [
-          "exemplify",
-          "exemplary"
-        ]
-      },
-      {
-        "definition": "an occurrence of something",
-        "partOfSpeech": "noun",
-        "synonyms": [
-          "case",
-          "instance"
-        ],
-        "typeOf": [
-          "happening",
-          "natural event",
-          "occurrence",
-          "occurrent"
-        ],
-        "hasTypes": [
-          "clip",
-          "mortification",
-          "piece",
-          "time",
-          "humiliation",
-          "bit"
-        ],
-        "derivation": [
-          "exemplify"
-        ],
-        "examples": [
-          "but there is always the famous example of the Smiths"
-        ]
-      },
-      {
-        "definition": "an item of information that is typical of a class or group",
-        "partOfSpeech": "noun",
-        "synonyms": [
-          "illustration",
-          "instance",
-          "representative"
-        ],
-        "typeOf": [
-          "information"
-        ],
-        "hasTypes": [
-          "excuse",
-          "apology",
-          "specimen",
-          "case in point",
-          "sample",
-          "exception",
-          "quintessence",
-          "precedent"
-        ],
-        "derivation": [
-          "exemplify",
-          "exemplary"
-        ],
-        "examples": [
-          "this patient provides a typical example of the syndrome",
-          "there is an example on page 10"
-        ]
-      },
-      {
-        "definition": "punishment intended as a warning to others",
-        "partOfSpeech": "noun",
-        "synonyms": [
-          "deterrent example",
-          "lesson",
-          "object lesson"
-        ],
-        "typeOf": [
-          "monition",
-          "admonition",
-          "word of advice",
-          "warning"
-        ],
-        "derivation": [
-          "exemplary"
-        ],
-        "examples": [
-          "they decided to make an example of him"
-        ]
-      },
-      {
-        "definition": "a task performed or problem solved in order to develop skill or understanding",
-        "partOfSpeech": "noun",
-        "synonyms": [
-          "exercise"
-        ],
-        "typeOf": [
-          "lesson"
-        ],
-        "examples": [
-          "you must work the examples at the end of each chapter in the textbook"
-        ]
-      }
-    ],
-    "syllables": {
-      "count": 3,
-      "list": [
-        "ex",
-        "am",
-        "ple"
-      ]
-    },
-    "pronunciation": {
-      "all": "ɪɡ'zæmpəl"
-    },
-    "frequency": 4.67
+let btn = document.getElementById("btn");
+let textinput = document.getElementById("text-input").value;
+// console.log(textinput);  
+
+// Pretend This response is coming from the sever
+const words = [
+  { word: "hello", definition: "hello or an equivalent greeting." },
+  {
+    word: "example", definition: "Something that is representative of all such things in a group."
   }
+];
+console.log(`hell ${words[0].word}`)
+
+// taking a input from the input tag
+function userinput() {
+  let textinput = document.getElementById("text-input").value;
+  document.getElementById("demo").innerHTML = textinput
+  if (textinput == words.word){
+    console.log(words.word)
+  }
+  else{
+    console.log("Hello word")
+  }
+
   
+    
+  
+  // console.log(textinput)
+  
+
+}
